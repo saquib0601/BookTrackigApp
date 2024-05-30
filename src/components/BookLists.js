914248759ef1c734bf5f7266825c94f01c578e33
saquib0601 <saquibmallik007@gmail.com>
2024-05-31 01:22:05 +0530

    Intilized project, Added tailwindcss, Added Login, Added BookCard

---
+++ src/components/BookLists.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import BookCard from './BookCard';
+
+const BooksList = () => {
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://www.googleapis.com/books/v1/volumes?q=fiction');
+        console.log(response)
+        console.log(response.data.items)
+        setBooks(response.data.items);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // if (error) {
+  //   return <div>Error: {error.message}</div>;
+  // }
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {books.map(book => (
+        <div key={book.id}>
+          <BookCard img={book.volumeInfo.imageLinks.thumbnail} 
+            title={book.volumeInfo.title} author={book.volumeInfo.authors ?? []}/>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BooksList;
---
+++ src/components/Login.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [formValue, setFormValue] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  return (
+    <Grid
+      container
+      justifyContent={"center"}
+      height={"100vh"}
+      alignItems={"center"}
+    >
+      <Grid
+        style={{ background: "white", width: "100%" }}
+        justifyContent={"center"}
+        alignItems={"center"}
+        container
+      >
+        <h1 className="text-3xl font-bold text-gray-800">Welcome Back</h1>
+        <Grid container item justifyContent={"center"} alignItems={"center"}>
+          <Grid
+            mt={2}
+            container
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <TextField
+              onChange={(e) => {
+                setFormValue({ ...formValue, userName: e.target.value });
+              }}
+              id="outlined-basic"
+              label="User Name"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid
+            mt={2}
+            container
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <TextField
+              onChange={(e) => {
+                setFormValue({ ...formValue, password: e.target.value });
+              }}
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+            />
+          </Grid>
+        </Grid>
+        <Grid ml={-9} container justifyContent={"center"} alignItems={"center"}>
+          <Button>Forget Password</Button>
+        </Grid>
+        <Grid mt={2} container justifyContent={"center"} alignItems={"center"}>
+          <Button
+            onClick={() => {
+              localStorage.setItem("userName", formValue.userName);
+              localStorage.setItem("isAuth", true);
+              navigate("/")
+            }}
+            variant="contained"
+          >
+            Login
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
