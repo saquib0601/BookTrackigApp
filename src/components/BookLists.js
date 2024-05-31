69c758bac2d88427bb7e3b96e87349478fea52c9
saquib0601 <saquibmallik007@gmail.com>
2024-06-01 01:47:14 +0530

    Added Custom hooks and Implement switch between grid and list

--- src/components/BookLists.js
+++ src/components/BookLists.js
@@ -2,38 +2,23 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import BookCard from "./BookCard";
 import Header from "./Header";
-import { DndProvider, useDrag, useDrop } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import useViewMode from "../hooks/useViewMode";
+import ViewModeToggle from "./ViewModeToggle";
+import useFetchBooks from "../hooks/useFetchBooks";
+import { Book_Collection_API } from "../utils/constants";
 
 const BooksList = () => {
-  const [books, setBooks] = useState([]);
+  const { data: books, isLoading, error } = useFetchBooks(Book_Collection_API);
+
   const [filteredBooks, setFilteredBooks] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
   const [searchQuery, setSearchQuery] = useState("");
+  const [isGridView, toggleViewMode] = useViewMode(true); // Using custom hook
 
   useEffect(() => {
     setFilteredBooks(books);
   }, [books]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://www.googleapis.com/books/v1/volumes?q=fiction"
-        );
-        console.log(response);
-        console.log(response.data.items);
-        setBooks(response.data.items);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   const handleSearch = (e) => {
     const query = e.target.value.toLowerCase();
@@ -57,44 +42,54 @@ const BooksList = () => {
     setFilteredBooks(updatedBooks);
   };
 
-  // if (isLoading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-  // if (error) {
-  //   return <div>Error: {error.message}</div>;
-  // }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <DndProvider backend={HTML5Backend}>
-    <div>
-      <Header />
-      <div className="p-4">
-        <input
-          type="text"
-          placeholder="Search Book..."
-          value={searchQuery}
-          onChange={handleSearch}
-          className="w-full p-2 border border-gray-300 rounded-lg"
-        />
+      <div>
+        <Header />
+        <div className="p-4">
+          <input
+            type="text"
+            placeholder="Search Book..."
+            value={searchQuery}
+            onChange={handleSearch}
+            className="w-full p-2 border border-gray-300 rounded-lg"
+          />
+          <ViewModeToggle
+            isGridView={isGridView}
+            toggleViewMode={toggleViewMode}
+          />
+        </div>
+        <div
+          className={
+            isGridView
+              ? "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ml-10"
+              : "flex flex-col items-center gap-4 ml-10"
+          }
+        >
+          {filteredBooks.map((book, index) => (
+            <div key={book.id}>
+              <BookCard
+                key={book.id}
+                index={index}
+                id={book.id}
+                description={book.volumeInfo.description}
+                img={book.volumeInfo.imageLinks.thumbnail}
+                title={book.volumeInfo.title}
+                author={book.volumeInfo.authors ?? []}
+                moveCard={moveCard}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ml-10">
-        {filteredBooks.map((book,index) => (
-          <div key={book.id}>
-            <BookCard
-              key={book.id}
-              index={index}
-              id={book.id}
-              description={book.volumeInfo.description}
-              img={book.volumeInfo.imageLinks.thumbnail}
-              title={book.volumeInfo.title}
-              author={book.volumeInfo.authors ?? []}
-              moveCard={moveCard}
-            />
-          </div>
-        ))}
-      </div>
-    </div>
     </DndProvider>
   );
 };
