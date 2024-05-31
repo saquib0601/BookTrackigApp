2eef2e49d07fe615c1e6a8e7e774569f710c647f
saquib0601 <saquibmallik007@gmail.com>
2024-05-31 10:25:23 +0530

    Added Dialog for each BookCard

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,18 +1,31 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import BookCardDialog from './dialog/BookCardDialog'
 
-export default function BookCard({img, title, author}) {
+export default function BookCard({img, title, author, description}) {
+
+  const [open, setOpen] = useState(false);
+
+  const handleBookCardDialog= () => {
+    setOpen(true);
+  }
+
+  function handleCloseDialog () {
+    setOpen(false);
+  }
 
   return (
+
     <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
+      {open && <BookCardDialog description={description} img={img} author={author} title={title}  open={open} handleClose={handleCloseDialog}/>}
+      <CardActionArea onClick={handleBookCardDialog}>
         <CardMedia
           component="img"
-          height="140"
+          height="100"
           image={img}
           alt="green iguana"
         />
--- src/components/BookLists.js
+++ src/components/BookLists.js
@@ -36,7 +36,7 @@ const BooksList = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {books.map(book => (
         <div key={book.id}>
-          <BookCard img={book.volumeInfo.imageLinks.thumbnail} 
+          <BookCard description={book.volumeInfo.description} img={book.volumeInfo.imageLinks.thumbnail} 
             title={book.volumeInfo.title} author={book.volumeInfo.authors ?? []}/>
         </div>
       ))}
